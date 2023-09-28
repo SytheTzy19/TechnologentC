e11a7f65d5c42d32f7552d727a936dcd364806e9
SytheTzy19 <131767987+SytheTzy19@users.noreply.github.com>
2023-09-28 17:00:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('explore').addEventListener('click', function() {
+    alert('Exploring the Technologent Company!');
+});
+function changeColor() {
+  var text = document.getElementById("text");
+  text.style.color = "red";
+}
+
